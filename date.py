a9e26f6b1c04719cf3c4869a6897fabe2170ce6f
Arsalan Rasheed <arsalan1mehar@gmail.com>
2021-10-24 13:11:32 +0500

    Task Complete

---
+++ date.py
@@ -0,0 +1,9 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=' ';
+print("Twinkle,twinkle, little star,\n"+
+      x*6+"How I wonder what you are!\n"+
+      x*12+"Up above the world so high,\n"+
+      x*12+"Like a diamond in the sky.\n"
+      "Twinkle, twinkle, little star,\n"+
+      x*6+"How I wonder what you are");
